53878d2142b6cae01427ca1393eee1f43b51a55d
omarabdelazizmoh <omar.abdelaziz94@gmail.com>
2022-10-07 20:49:46 -0700

    Human move

--- human_robot_interaction/src/go_to_goal.py
+++ human_robot_interaction/src/go_to_goal.py
@@ -22,10 +22,12 @@ class Husky:
         # when a message of type Pose is received.
         self.pose_subscriber = rospy.Subscriber('/husky_velocity_controller/odom', Odometry, self.update_pose)
 
-        self.goals_X = [1,2,3]
-        self.goals_Y = [0,0,0]
+        self.goals_X = [20,0,0,0,0]
+        self.goals_Y = [0,0,0,0,0]
 
-        self.sub = rospy.Subscriber('/gazebo/model_states', ModelStates, self.callback)                                                
+        self.sub = rospy.Subscriber('/gazebo/model_states', ModelStates, self.callback)         
+
+        self.waypoints_flag = 0                                       
 
         # We used Pose() message for the self.robot_pose because the Odometry msg is bigger than needed, so we saved the values of the variables we're interested in from the Odometry msg into this Pose msg
         self.robot_pose = Pose()
@@ -35,15 +37,48 @@ class Husky:
     def callback(self, data):
 
         # Extracting positon info from /gazebo/model_states
-        human_posX = data.pose[2].position.x
-        human_posY = data.pose[2].position.y
+        self.human_pose = Pose()
+        self.human_pose.x = data.pose[2].position.x
+        self.human_pose.y = data.pose[2].position.y
 
-        # print("human_posX: ", human_posX)
-        # print("human_posY: ", human_posY)
+        # robot_posX = data.pose[3].position.x
+        # robot_posY = data.pose[3].position.y
+        # print("Robot PosX: ", robot_posX)
+        # print("Robot PosY: ", robot_posY)
+        
+        # Distance between robot and human
+        # dist = abs(human_pose.x - robot_posX)
+        dist = self.euclidean_distance(self.human_pose)
+        # print("Eucl dist = ", dist)
+        
+        # Defining safe position for robot to move to when coming close to human in the passing scenario
+        # self.safe_posX = human_pose.x
+        # self.safe_posY = human_pose.y - 3
 
-        # Target points
-        self.goals_X = [human_posX - 3, human_posX, human_posX + 3]
-        self.goals_Y = [human_posY, human_posY - 3, human_posY]
+        # Robot moving/stopping conditions
+        if(dist < 3):
+            
+            # Setting robot speeds
+            self.goals_X = [self.human_pose.x - 3,   self.human_pose.x - 2,       self.human_pose.x,          self.human_pose.x + 2,     self.human_pose.x + 3]
+            self.goals_Y = [self.human_pose.y,       self.human_pose.y - 2,       self.human_pose.y - 3,      self.human_pose.y - 2,     self.human_pose.y - 0.5]
+            # print("Distance is less than 3.")
+
+            self.waypoints_flag = 1
+            self.move2goal()
+
+        else:
+            # Setting robot speeds
+            vel_msg = Twist()
+            vel_msg.linear.x = 1
+            self.velocity_publisher.publish(vel_msg)
+            # print("Keep going.")
+
+        # print("human_pose.x: ", human_pose.x)
+        # print("human_pose.y: ", human_pose.y)
+
+        # # Target points
+        # self.goals_X = [human_pose.x - 3, human_pose.x, human_pose.x + 3]
+        # self.goals_Y = [human_pose.y, human_pose.y - 3, human_pose.y]
 
 
     def update_pose(self, data):
@@ -61,10 +96,9 @@ class Husky:
 
     def euclidean_distance(self, goal_pose):
         """Euclidean distance between current pose and the goal."""
-        return sqrt(pow((goal_pose.x - self.robot_pose.x), 2) +
-                    pow((goal_pose.y - self.robot_pose.y), 2))
+        return sqrt(pow((goal_pose.x - self.robot_pose.x), 2) + pow((goal_pose.y - self.robot_pose.y), 2))
 
-    def linear_vel(self, goal_pose, constant=0.5):
+    def linear_vel(self, goal_pose, constant=0.3):
         """See video: https://www.youtube.com/watch?v=Qh15Nol5htM."""
         return constant * self.euclidean_distance(goal_pose)
 
@@ -72,78 +106,104 @@ class Husky:
         """See video: https://www.youtube.com/watch?v=Qh15Nol5htM."""
         return atan2(goal_pose.y - self.robot_pose.y, goal_pose.x - self.robot_pose.x)
 
-    def angular_vel(self, goal_pose, constant=1):
+    def angular_vel(self, goal_pose, constant=1.2):
         """See video: https://www.youtube.com/watch?v=Qh15Nol5htM."""
         return constant * (self.steering_angle(goal_pose) - self.robot_pose.theta)
 
     def move2goal(self):
         """Moves the turtle to the goal."""
-        goal_pose = Pose()
 
-        for i in range( len(self.goals_X) ):
+        print("flag = ", self.waypoints_flag)
+        
+        if(self.waypoints_flag == 1):
 
-            # Get the input from the user.
-            goal_pose.x = self.goals_X[i]
-            goal_pose.y = self.goals_Y[i]
-            
-            # Please, insert a number slightly greater than 0 (e.g. 0.01).
-            distance_tolerance = 0.5
+            goal_pose = Pose()
 
-            vel_msg = Twist()
-            # print("For loop")
+            for i in range( len(self.goals_X) ):
 
-            while self.euclidean_distance(goal_pose) >= distance_tolerance:
-
-                # Porportional controller.
-                # https://en.wikipedia.org/wiki/Proportional_control
-
-                # Calculating errors
-                self.dist_error_X = goal_pose.x - self.robot_pose.x
-                self.dist_error_Y = goal_pose.y - self.robot_pose.y
-                self.orientation_error = self.steering_angle(goal_pose) - self.robot_pose.theta
+                # Get the input from the user.
+                goal_pose.x = self.goals_X[i]
+                goal_pose.y = self.goals_Y[i]
+                # print("goal_pose updated.")
                 
-                # Print errors    
-                # print("Error in X: ", self.dist_error_X)
-                # print("Error in Y: ", self.dist_error_Y)
-                # print("Error in Orientation: ", self.orientation_error)
+                # Please, insert a number slightly greater than 0 (e.g. 0.01).
+                distance_tolerance = 0.3
 
-                # Linear velocity in the x-axis.
-                vel_msg.linear.x = self.linear_vel(goal_pose)
-                vel_msg.linear.y = 0
-                vel_msg.linear.z = 0
+                vel_msg = Twist()
+                # print("For loop")
 
-                # Angular velocity in the z-axis.
-                vel_msg.angular.x = 0
-                vel_msg.angular.y = 0
-                vel_msg.angular.z = self.angular_vel(goal_pose)
-
-                # Publishing our vel_msg
-                self.velocity_publisher.publish(vel_msg)
-
-                # Publish at the desired rate.
-                # self.rate.sleep()
-
+                if(self.euclidean_distance(goal_pose)) > 3:
                     
-            # Stopping our robot after the movement is over.
-            vel_msg.linear.x = 0
-            vel_msg.angular.z = 0
-            self.velocity_publisher.publish(vel_msg)
-            print("Reached goal ", i)
+                    # Linear velocity in the x-axis.
+                        vel_msg.linear.x = 1
+                        vel_msg.linear.y = 0
+                        vel_msg.linear.z = 0
 
-            # Get the input from the user.
-            goal_pose.x = self.goals_X[i]
-            goal_pose.y = self.goals_Y[i]
+                else:
 
-            if(i == 2):
-                rospy.signal_shutdown("All goals reached.")
+                    while self.euclidean_distance(goal_pose) >= distance_tolerance:
 
-            # If we press control + C, the node will stop.
-            # rospy.spin()
+                        # Porportional controller.
+                        # https://en.wikipedia.org/wiki/Proportional_control
+
+                        # Calculating errors
+                        self.dist_error_X = goal_pose.x - self.robot_pose.x
+                        self.dist_error_Y = goal_pose.y - self.robot_pose.y
+                        self.orientation_error = self.steering_angle(goal_pose) - self.robot_pose.theta
+                        
+                        # Print errors    
+                        # print("Error in X: ", self.dist_error_X)
+                        # print("Error in Y: ", self.dist_error_Y)
+                        # print("Error in Orientation: ", self.orientation_error)
+
+                        # Linear velocity in the x-axis.
+                        vel_msg.linear.x = self.linear_vel(goal_pose)
+                        vel_msg.linear.y = 0
+                        vel_msg.linear.z = 0
+
+                        # Angular velocity in the z-axis.
+                        vel_msg.angular.x = 0
+                        vel_msg.angular.y = 0
+                        vel_msg.angular.z = self.angular_vel(goal_pose)
+
+                        # Publishing our vel_msg
+                        self.velocity_publisher.publish(vel_msg)
+
+                        # Publish at the desired rate.
+                        # self.rate.sleep()
+
+                            
+                    # Stopping our robot after the movement is over.
+                    vel_msg.linear.x = -1
+                    vel_msg.angular.z = 0
+                    self.velocity_publisher.publish(vel_msg)
+                    print("Reached goal ", i)
+                    print("Goal pos X = ", self.goals_X[i])
+                    print("Goal pos X = ", self.goals_Y[i])
+                    print("Human pose X = ", self.human_pose.x)
+                    print("Human pose Y = ", self.human_pose.y)
+                    print("Robot pose X = ", self.robot_pose.x)
+                    print("Robot pose Y = ", self.robot_pose.y)
+                    print("---------------------------")
+                    
+
+                    # Get the input from the user.
+                    goal_pose.x = self.goals_X[i]
+                    goal_pose.y = self.goals_Y[i]
+
+                    if(i == (len(self.goals_X) - 1)):
+                        vel_msg.linear.x = 0
+                        vel_msg.angular.z = 0
+                        self.velocity_publisher.publish(vel_msg)
+                        rospy.signal_shutdown("All goals reached.")
+
+                    # If we press control + C, the node will stop.
+                    # rospy.spin()
 
 if __name__ == '__main__':
     try:
-        x = Husky()
-        x.move2goal()
+        Husky()
+        # x.move2goal()
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
---
+++ human_robot_interaction/src/human_move.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import rospy
+from human_robot_interaction.msg import Act_msg
+
+def main():
+
+    rospy.init_node('move_human')
+    
+    pub = rospy.Publisher('actor1/vel_cmd', Act_msg, queue_size=10)
+
+    rate = rospy.Rate(10) 
+    while not rospy.is_shutdown():
+        
+            # Publisher 
+            cmd_vel = Act_msg()
+            # cmd_vel.target_x = current_x + x
+            # cmd_vel.target_y = current_y + y
+            cmd_vel.velocity = 1.0
+
+            pub.publish(cmd_vel)
+    
+
+if __name__ =='__main__':
+    try:
+        main()
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
+
