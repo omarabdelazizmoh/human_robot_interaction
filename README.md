de389144de8a4e53236d26b9d6ccf9f1a7fe0f7c
noshluk2 <noshluk2@gmail.com>
2023-01-13 09:32:20 +0500

    Static occupied circle on map

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
 # Repository for Human Robot Interaction based Project
 
-## Installations
-```
-sudo apt-get install ros-noetic-husky-*
-sudo apt-get install ros-noetic-navigation
-sudo apt-get install ros-noetic-gazebo-ros
-sudo apt-get install ros-noetic-gazebo-plugins
-```
-## Commands
+## Current Development
+- **actor_to_occupancyGrid.py** : Add Dynamic human motion as occupied cells in static free map-> code is written debugging is needed
+### Test it
+-   Mapping Occupancy Grid from Human Location
+    - Node is currently incomplete and publishes static Map
+    ```
+    roslaunch human_robot_interaction human_behavior_with_husky.launch test:=true
+
+    ```
+    ```
+    rosrun human_robot_interaction actor_to_occupancyGrid.py
+    ```
+
+
+## Working Features
 - To bring human and husky in simulation
     - Passing Scenario
     ```
@@ -24,7 +31,17 @@ sudo apt-get install ros-noetic-gazebo-plugins
     roslaunch human_robot_interaction human_behavior_with_husky.launch overtaking:=true
 
     ```
-## Files Explaination
+
+
+## Installations
+```
+sudo apt-get install ros-noetic-husky-*
+sudo apt-get install ros-noetic-navigation
+sudo apt-get install ros-noetic-gazebo-ros
+sudo apt-get install ros-noetic-gazebo-plugins
+```
+
+## Files Explaination ( OLD )
 
 Run the launch file "human_behavior_detection.launch". This launch file runs the following:
 
@@ -34,14 +51,3 @@ Run the launch file "human_behavior_detection.launch". This launch file runs the
 4. Runs the "state_recognition.py" node.
 5. Runs the "robot_interaction.py" node.
 
----------------------------------------------------------------------------------------------------------------
-- The husky robot is by default spawned at the origin, but that can be changed by editing the argument defined in the launch file
-```
-human_behavior_with_husky.launch
-```
-
-- The "human_move.py" node defines a target position (x,y) for the human to move to, as well as a velocity to move with to that target.
-
-- The "state_recognition.py" node is used to identify the scenario that the human and robot are experiencing at any given moment. These scenarios may either be "passing" or "crossing" scenarios.
-
-- The "robot_interaction.py" node controls the robot's motion in both the passing and crossing scenarios in order to safely avoid the human.
---
+++ human_robot_interaction/src/actor_to_occupancyGrid.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+import rospy
+from nav_msgs.msg import OccupancyGrid
+from gazebo_msgs.msg import ModelStates
+from turtlesim.msg import Pose
+
+
+class actor_occupancy_grid():
+    def __init__(self):
+        rospy.init_node('og_from_actor', anonymous=True)
+        # Create the publisher
+        self.map_publisher= rospy.Publisher('map', OccupancyGrid, queue_size=10)
+        self.model_states_subscriber = rospy.Subscriber('/gazebo/model_states', ModelStates, self.callback)
+        # Set the rate at which messages will be published
+        self.grid = OccupancyGrid()
+        self.human_pose = Pose()
+
+    def callback(self, data):
+        self.human_pose.x = data.pose[1].position.x
+        self.human_pose.y = data.pose[1].position.y
+
+            # Fill the grid message with data
+        self.grid.header.stamp = rospy.Time.now()
+        self.grid.header.frame_id = "map_tf"
+        self.grid.info.resolution = 0.05
+        self.grid.info.width = 100
+        self.grid.info.height = 100
+        self.grid.info.origin.position.x = 0
+        self.grid.info.origin.position.y = 0
+        self.grid.data = [0] * (self.grid.info.width * self.grid.info.height)
+
+        # Fill the data around a point as occupied
+        center_x = 50
+        center_y = 50
+        radius = 5
+        for x in range(self.grid.info.width):
+            for y in range(self.grid.info.height):
+                if ((x - center_x) ** 2 + (y - center_y) ** 2) <= radius ** 2:
+                    self.grid.data[y * self.grid.info.width + x] = 100
+
+        # Working on Dynamic HUman Location updating in Occupancy grid
+
+        # x = int((self.human_pose.x - self.grid.info.origin.position.x) / self.grid.info.resolution)
+        # y = int((self.human_pose.y - self.grid.info.origin.position.y) / self.grid.info.resolution)
+        # index = y * self.grid.info.width + x
+        # data[index] = 100  # Mark the cell where the robot is located as occupied
+        # self.grid.data = data.tolist()
+
+
+        # Publish the message
+        self.map_publisher.publish(self.grid)
+        # print("H_X | ", self.human_pose.x , "//  H_Y | " , self.human_pose.y)
+
+
+if __name__ == '__main__':
+    try:
+        actor_occupancy_grid()
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
